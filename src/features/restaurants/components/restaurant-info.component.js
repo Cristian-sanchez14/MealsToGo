40e1ccf5250eec5cf7a3fe2f2e5c99a71069cdc5
Cristian Sanchez <cristiansanchez@Cristians-MacBook-Pro.local>
2023-11-26 17:08:24 -0700

    changing file structure and adding components and screens

---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const RestaurantInfo = () => {
+  return (
+    <View>
+      <Text>Restaurant Info</Text>
+    </View>
+  )
+}
+
