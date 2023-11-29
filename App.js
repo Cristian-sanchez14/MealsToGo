131649f3a4318fc6c32a3ffc796ebbc411f6e802
Cristian Sanchez <cristiansanchez@Cristians-MacBook-Pro.local>
2023-11-29 09:00:46 -0700

    adding firebase

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+// firebase
+import * as firebase from "firebase";
+
 
 import {
   useFonts as useOswald,
@@ -14,6 +17,26 @@ import { Navigation } from "./src/infrastructure/navigation";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATsqGgoibanDBYJegLrA4N2vkyP_8UJvk",
+  authDomain: "mealstogoapp-ff045.firebaseapp.com",
+  projectId: "mealstogoapp-ff045",
+  storageBucket: "mealstogoapp-ff045.appspot.com",
+  messagingSenderId: "109071745307",
+  appId: "1:109071745307:web:dc636f37b1fa1aab3e53be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
