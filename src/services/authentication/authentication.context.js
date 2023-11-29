4cb52ab83015f8f91c0339017111c03b7c3379aa
Cristian Sanchez <cristiansanchez@Cristians-MacBook-Pro.local>
2023-11-29 16:24:23 -0700

    favourites multiuser part 1

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,10 +1,5 @@
-import React, { useState, createContext, useRef } from "react";
-import {
-  signOut,
-  createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  getAuth,
-} from "firebase/auth";
+import React, { useState, createContext } from "react";
+import * as firebase from "firebase";
 
 import { loginRequest } from "./authentication.service";
 
@@ -14,9 +9,8 @@ export const AuthenticationContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
-  const auth = useRef(getAuth()).current;
 
-  onAuthStateChanged(auth, (usr) => {
+  firebase.auth().onAuthStateChanged((usr) => {
     if (usr) {
       setUser(usr);
       setIsLoading(false);
@@ -25,10 +19,9 @@ export const AuthenticationContextProvider = ({ children }) => {
     }
   });
 
-  // lOGIN 
   const onLogin = (email, password) => {
     setIsLoading(true);
-    loginRequest(auth, email, password)
+    loginRequest(email, password)
       .then((u) => {
         setUser(u);
         setIsLoading(false);
@@ -39,14 +32,15 @@ export const AuthenticationContextProvider = ({ children }) => {
       });
   };
 
-  // REGISTER 
   const onRegister = (email, password, repeatedPassword) => {
     setIsLoading(true);
     if (password !== repeatedPassword) {
       setError("Error: Passwords do not match");
       return;
     }
-    createUserWithEmailAndPassword(auth, email, password)
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
       .then((u) => {
         setUser(u);
         setIsLoading(false);
@@ -58,10 +52,8 @@ export const AuthenticationContextProvider = ({ children }) => {
   };
 
   const onLogout = () => {
-    signOut(auth).then(() => {
-      setUser(null);
-      setError(null);
-    });
+    setUser(null);
+    firebase.auth().signOut();
   };
 
   return (
