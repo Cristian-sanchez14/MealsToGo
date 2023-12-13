b0c0ebed2f222c411d1f18a7874825eec8951d5d
Cristian Sanchez <cristiansanchez@Cristians-MacBook-Pro.local>
2023-12-13 10:12:24 -0700

    handling error state for checkout.screen

--- src/services/cart/cart.context.js
+++ src/services/cart/cart.context.js
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { AuthenticationContext } from "../authentication/authentication.context";
 
-export const CartContext = createContext();
+export const CartContext = createContext(); // pulls information or context
 
 export const CartContextProvider = ({ children }) => {
   const { user } = useContext(AuthenticationContext);
@@ -11,66 +11,24 @@ export const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [restaurant, setRestaurant] = useState(null);
 
-  const [sum, setSum] = useState(0);
-
-  const saveCart = async (rst, crt, uid) => {
-    try {
-      const jsonValue = JSON.stringify({ restaurant: rst, cart: crt });
-      await AsyncStorage.setItem(`@cart-${uid}`, jsonValue);
-    } catch (e) {
-      console.log("error storing", e);
-    }
-  };
-
-  const loadCart = async (uid) => {
-    try {
-      const value = await AsyncStorage.getItem(`@cart-${uid}`);
-      if (value !== null) {
-        const { restaurant: rst, cart: crt } = JSON.parse(value);
-        setRestaurant(rst);
-        setCart(crt);
-      }
-    } catch (e) {
-      console.log("error storing", e);
-    }
-  };
-
-  useEffect(() => {
-    if (user && user.uid) {
-      loadCart(user.uid);
-    }
-  }, [user]);
-
-  useEffect(() => {
-    if (user && user.uid) {
-      saveCart(restaurant, cart, user.uid);
-    }
-  }, [restaurant, cart, user]);
-
-  useEffect(() => {
-    if (!cart.length) {
-      setSum(0);
-      return;
-    }
-    const newSum = cart.reduce((acc, { price }) => {
-      return (acc += price);
-    }, 0);
-    setSum(newSum);
-  }, [cart]);
-
+  // function that adds to cart
   const add = (item, rst) => {
     if (!restaurant || restaurant.placeId !== rst.placeId) {
       setRestaurant(rst);
-      setCart([item]);
+      setCart([item]); // adding from menu
     } else {
       setCart([...cart, item]);
-    }
+    } // adding more on the cart
   };
+  
+  // const remove = (item) => {
+  //   cart.filter(i => item.id)
+  // }; // functionality is not added to the app
 
   const clear = () => {
     setCart([]);
     setRestaurant(null);
-  };
+  }; // clearing cart 
 
   return (
     <CartContext.Provider
@@ -79,7 +37,6 @@ export const CartContextProvider = ({ children }) => {
         clearCart: clear,
         restaurant,
         cart,
-        sum,
       }}
     >
       {children}
